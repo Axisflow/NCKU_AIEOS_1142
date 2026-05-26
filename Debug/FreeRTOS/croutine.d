39575229a331b04b6dc0cf720bea8cb9475d4c92
Candice <candiceliu239@gmail.com>
2026-05-26 21:12:54 +0800

    Project initialization

---
+++ Debug/FreeRTOS/croutine.d
@@ -0,0 +1,21 @@
+FreeRTOS/croutine.o: ../FreeRTOS/croutine.c \
+ D:/Embedded\ System/lab0/FreeRTOS/include/FreeRTOS.h \
+ D:/Embedded\ System/lab0/FreeRTOS/include/FreeRTOSConfig.h \
+ D:/Embedded\ System/lab0/FreeRTOS/include/projdefs.h \
+ D:/Embedded\ System/lab0/FreeRTOS/include/portable.h \
+ D:/Embedded\ System/lab0/FreeRTOS/include/deprecated_definitions.h \
+ D:/Embedded\ System/lab0/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ D:/Embedded\ System/lab0/FreeRTOS/include/mpu_wrappers.h \
+ D:/Embedded\ System/lab0/FreeRTOS/include/task.h \
+ D:/Embedded\ System/lab0/FreeRTOS/include/list.h \
+ D:/Embedded\ System/lab0/FreeRTOS/include/croutine.h
+D:/Embedded\ System/lab0/FreeRTOS/include/FreeRTOS.h:
+D:/Embedded\ System/lab0/FreeRTOS/include/FreeRTOSConfig.h:
+D:/Embedded\ System/lab0/FreeRTOS/include/projdefs.h:
+D:/Embedded\ System/lab0/FreeRTOS/include/portable.h:
+D:/Embedded\ System/lab0/FreeRTOS/include/deprecated_definitions.h:
+D:/Embedded\ System/lab0/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+D:/Embedded\ System/lab0/FreeRTOS/include/mpu_wrappers.h:
+D:/Embedded\ System/lab0/FreeRTOS/include/task.h:
+D:/Embedded\ System/lab0/FreeRTOS/include/list.h:
+D:/Embedded\ System/lab0/FreeRTOS/include/croutine.h:
---
+++ Debug/FreeRTOS/portable/MemMang/heap_4.d
@@ -0,0 +1,19 @@
+FreeRTOS/portable/MemMang/heap_4.o: ../FreeRTOS/portable/MemMang/heap_4.c \
+ D:/Embedded\ System/lab0/FreeRTOS/include/FreeRTOS.h \
+ D:/Embedded\ System/lab0/FreeRTOS/include/FreeRTOSConfig.h \
+ D:/Embedded\ System/lab0/FreeRTOS/include/projdefs.h \
+ D:/Embedded\ System/lab0/FreeRTOS/include/portable.h \
+ D:/Embedded\ System/lab0/FreeRTOS/include/deprecated_definitions.h \
+ D:/Embedded\ System/lab0/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ D:/Embedded\ System/lab0/FreeRTOS/include/mpu_wrappers.h \
+ D:/Embedded\ System/lab0/FreeRTOS/include/task.h \
+ D:/Embedded\ System/lab0/FreeRTOS/include/list.h
+D:/Embedded\ System/lab0/FreeRTOS/include/FreeRTOS.h:
+D:/Embedded\ System/lab0/FreeRTOS/include/FreeRTOSConfig.h:
+D:/Embedded\ System/lab0/FreeRTOS/include/projdefs.h:
+D:/Embedded\ System/lab0/FreeRTOS/include/portable.h:
+D:/Embedded\ System/lab0/FreeRTOS/include/deprecated_definitions.h:
+D:/Embedded\ System/lab0/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+D:/Embedded\ System/lab0/FreeRTOS/include/mpu_wrappers.h:
+D:/Embedded\ System/lab0/FreeRTOS/include/task.h:
+D:/Embedded\ System/lab0/FreeRTOS/include/list.h:
