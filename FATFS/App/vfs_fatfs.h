96278f8c9f50a8f5d12a1260bfb6b72aa70645fa
Axisflow <axisflow@hotmail.com>
2026-06-03 23:07:40 +0800

    feat: add C++ compatibility to VFS header files and update function to static declarations

--- FATFS/App/vfs_fatfs.h
+++ FATFS/App/vfs_fatfs.h
@@ -7,6 +7,10 @@
 #include "semphr.h"
 #include "ff.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // Inherit from 'struct file_system' and add private data for FATFS
 struct fat_fs {
     struct file_system base; // Base file system structure
@@ -18,16 +22,8 @@ struct fat_fs {
 int mount_fatfs(struct fat_fs *fs, const char *mount_point);
 int unmount_fatfs(struct fat_fs *fs);
 
-int fatfs_open(struct file *file, const char *path);
-__vf_ssize_t fatfs_read(struct file *file, char *buf, size_t count);
-__vf_ssize_t fatfs_write(struct file *file, const char *buf, size_t count);
-loff_t fatfs_llseek(struct file *file, loff_t offset, int whence);
-int fatfs_fsync(struct file *file, loff_t start, loff_t end, int datasync);
-poll_t fatfs_poll(struct file *file, poll_t events, int timeout_ms);
-loff_t fatfs_iterate_shared (struct file *file, char *path, size_t path_max_len);
-int fatfs_close(struct file *file);
-
-int fatfs_create(const struct file_system *fs, const char *path, const char *name, umode_t mode);
-int fatfs_unlink(const struct file_system *fs, const char *path);
+#ifdef __cplusplus
+}
+#endif
 
 #endif // VFS_FATFS_H
