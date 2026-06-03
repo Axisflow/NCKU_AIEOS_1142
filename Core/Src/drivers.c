13a02f6862f7270d02a269ba98d4bcf84a12f92e
yang <n26144214@gs.ncku.edu.tw>
2026-06-03 23:12:12 +0800

    feat(driver): implement DWT microsecond delay

--- Core/Src/drivers.c
+++ Core/Src/drivers.c
@@ -224,6 +224,18 @@ void initialize_DHT22(void)
 {
 	DHT22_GPIO_CLK_ENABLE();
 	DHT22_SetPinInput();
+
+	// Enable DWT Cycle Counter
+	CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+	DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
 }
+
+void delay_us(uint32_t us)
+{
+	uint32_t start = DWT->CYCCNT;
+	uint32_t ticks = us * (SystemCoreClock / 1000000);
+	while ((DWT->CYCCNT - start) < ticks);
+}
+
 
 
